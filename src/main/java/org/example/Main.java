72f94e7d4effedadf493c8f6bb31c3486aedeba5
Ömer Fatih Aslan <omrfth23@gmail.com>
2026-02-17 01:24:07 +0300

    feat: refactor Main class methods and add main method for testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,63 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+    public static boolean shouldWakeUp(boolean barking, int hour) {
+        if (barking && (hour < 0 || hour > 23)) {
+            return false;
+        }
+        return barking && (hour < 8 || hour == 0);
     }
 
-    public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+
+    public static boolean hasTeen(int a, int b, int c) {
+        return (a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19);
     }
 
-    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer) {
+            return temperature >= 25 && temperature <= 45;
+        } else {
+            return temperature >= 25 && temperature <= 35;
+        }
     }
 
-    public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
-    }
 
-    public static double area(double width, double height) {
-       
+    public static double area(double length, double width) {
+        if (length < 0 || width < 0) {
+            return -1;
+        }
+        return length * width;
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+        System.out.println(shouldWakeUp(true, -1));
+
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
     }
 }
+
